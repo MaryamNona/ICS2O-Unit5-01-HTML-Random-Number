bc4ff70b81940032e6687fb02f835a375c0ac071
MaryamNona <>
2021-06-02 17:53:41 +0000

    added index.html

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ICS2O-Unit5-01-HTML-Random-Number
+
+[![GitHub's Super Linter](https://github.com/MaryamNona/ICS2O-Unit5-01-HTML-Random-Number/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/MaryamNona/ICS2O-Unit5-01-HTML-Random-Number/actions)
+
+This website can be found at: [https://MaryamNona.github.io/ICS2O-Unit5-01-HTML-Random-Number](https://MaryamNona.github.io/ICS2O-Unit5-01-HTML-Random-Number)
---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Maryam Nona
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+"use strict"
+
+const number = Math.floor(Math.random() * 10) + 1
+//console.log(number)
+  
+function addAgeClicked () {
+  // this function shows a random number 
+
+  // input
+  const user_guess = parseInt(document.getElementById("random-guess").value)
+
+  // proces
+  if (number == user_guess) {
+    document.getElementById('number').innerHTML = 'You chose correctly!'
+  } else if (number > user_guess) {
+    document.getElementById('number').innerHTML = 'Your number is too low.'
+  } else if (number < user_guess) {
+    document.getElementById('number').innerHTML = 'Your number is too high.'
+  }
+}
